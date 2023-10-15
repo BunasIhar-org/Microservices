5eda15a1d6998235ebb9a79a2b0c3c4479e6b344
BunasIhar <136170643+BunasIhar@users.noreply.github.com>
2023-10-15 10:56:05 +0300

    Update AdminController.java

--- Task3.1.2/src/main/java/com/it/mentor/org/springbootsecuritydemo/controllers/AdminController.java
+++ Task3.1.2/src/main/java/com/it/mentor/org/springbootsecuritydemo/controllers/AdminController.java
@@ -30,17 +30,15 @@ public class AdminController {
     @GetMapping("/edit")
     public String editPerson(@RequestParam("id") Integer id, Model model) {
         Person person = adminService.findPersonById(id);
-        Set<Role> allRoles = new HashSet<>(Set.of(Role.ROLE_ADMIN, Role.ROLE_USER));
         model.addAttribute("person", person);
-        model.addAttribute("allRoles", allRoles);
+        model.addAttribute("allRoles", Role.values());
         return "admin/edit";
     }
 
     @PostMapping("/update")
     public String updateUser(Model model, @ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            Set<Role> allRoles = new HashSet<>(Set.of(Role.ROLE_ADMIN, Role.ROLE_USER));
-            model.addAttribute("allRoles", allRoles);
+            model.addAttribute("allRoles", Role.values());
             return "admin/edit";
         }
         adminService.updatePerson(person);
